e8a5232d7213218964ecc4a3021c9f0520837ebd
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-05-14 15:24:29 -0600

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,17 @@
+from random import choice, randint
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Well, this is awkward...'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Great, thanks!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {randint(1,6)}'
+    else:
+        return choice(['I do not quite understand...',
+                       'What are you talking about?',
+                       'Do you mind rephrasing that?'])
