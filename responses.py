3bccab69321d20acf909f9448371b9ec13f9b668
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-05-15 20:35:57 -0600

    Update responses.py

--- responses.py
+++ responses.py
@@ -11,6 +11,9 @@ def get_response(user_input: str) -> str:
         return 'Great, thanks!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1,6)}'
+    elif 'flip a coin' in lowered:
+        result = random.choice(['heads', 'tails'])
+        await message.author.send(f'You got: {result}')
     else:
         return choice(['I do not quite understand...',
                        'What are you talking about?',
