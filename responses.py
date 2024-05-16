c0be1b1c9b6c88975cf3a72650a592823cfdd4f2
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-05-15 21:03:18 -0600

    Update responses.py

--- responses.py
+++ responses.py
@@ -10,11 +10,26 @@ def get_response(user_input: str) -> str:
     elif 'how are you' in lowered:
         return 'Great, thanks!'
     elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1,6)}'
+        return f'You rolled: {roll_dice()}'
     elif 'flip a coin' in lowered:
-        result = random.choice(['heads', 'tails'])
-        await message.author.send(f'You got: {result}')
+        return f'You got: {flip_coin(user_input)}'
     else:
-        return choice(['I do not quite understand...',
+        return choose_random_response(lowered)  
+        
+        choice(['I do not quite understand...',
                        'What are you talking about?',
                        'Do you mind rephrasing that?'])
+
+def roll_dice() -> int:
+    return random.randint(1,6)
+
+def flip_coin() -> str:
+    result = random.choice(['heads', 'tails'])
+
+def choose_random_response(user_input: str) -> str:
+    responses: Final[str] = [
+        'I do not quite understand...',
+                       'What are you talking about?',
+                       'Do you mind rephrasing that?
+    ]
+    return random.choice(responses) if user_input else 'You didn't say anything'
