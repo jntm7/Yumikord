a3a8f35b488249f3c0be4258713557363eae4529
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-26 20:03:28 -0600

    Create general.py

---
+++ src/utils/general.py
@@ -0,0 +1,9 @@
+
+# No Message
+def no_message_response():
+    return 'Well, this is awkward...'
+
+# Bug Response
+def bug_response():
+    return ('For feature requests and bug reports, please review the [GitHub documentation](<https://github.com/jntm7/yumikord>). ''\nIf it is unable to be resolved, feel free to [open a new issue](<https://github.com/jntm7/Yumikord/issues>).''\nPlease ensure the respective labels `enhancement` for feature requests and `bug` for issues are assigned.')
+
