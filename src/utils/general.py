c8d226d29e7aee83fd934981541deccb32cadc1d
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-27 11:46:33 -0600

    Update general.py

--- src/utils/general.py
+++ src/utils/general.py
@@ -1,9 +1,15 @@
+# Bug Response
+def bug_response():
+    return ('For feature requests and bug reports, please review the [GitHub documentation](<https://github.com/jntm7/yumikord>). ''\nIf it is unable to be resolved, feel free to [open a new issue](<https://github.com/jntm7/Yumikord/issues>).''\nPlease ensure the respective labels `enhancement` for feature requests and `bug` for issues are assigned.')
 
 # No Message
 def no_message_response():
     return 'Well, this is awkward...'
 
-# Bug Response
-def bug_response():
-    return ('For feature requests and bug reports, please review the [GitHub documentation](<https://github.com/jntm7/yumikord>). ''\nIf it is unable to be resolved, feel free to [open a new issue](<https://github.com/jntm7/Yumikord/issues>).''\nPlease ensure the respective labels `enhancement` for feature requests and `bug` for issues are assigned.')
-
+# Random Greeting
+def choose_random_response(command):
+    responses = {
+        "greet": ["Hello!", "Hi!", "Hey!"],
+        "bye": ["Goodbye!", "See you!", "Bye!"]
+    }
+    return responses.get(command, ["I don't understand."])[0]
