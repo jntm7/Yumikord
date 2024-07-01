9271079baece6ec491d24d7edb2d59f4f5a6536f
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-30 18:35:35 -0600

    Update general.py

--- src/utils/general.py
+++ src/utils/general.py
@@ -1,6 +1,6 @@
 # Bug Response
 def bug_response():
-    return ('For feature requests and bug reports, please review the [GitHub documentation](<https://github.com/jntm7/yumikord>). ''\nIf it is unable to be resolved, feel free to [open a new issue](<https://github.com/jntm7/Yumikord/issues>).''\nPlease ensure the respective labels `enhancement` for feature requests and `bug` for issues are assigned.')
+    return ('For bug reports, please first review the [GitHub documentation](<https://github.com/jntm7/yumikord>).''\nIf it is unable to be resolved, feel free to [open a new issue](<https://github.com/jntm7/Yumikord/issues>).''\nPlease ensure the respective labels `enhancement` for feature requests and `bug` for issues are assigned.''\nFor feature requests and general inquiries, feel free to contribute to the [discussions page](https://github.com/jntm7/Yumikord/discussions).')
 
 # No Message
 def no_message_response():
