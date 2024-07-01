a8f9583197960eac21746c95b5220a2aa5b82bb8
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-06-30 21:33:21 -0600

    Update profile_commands.py

--- src/commands/profile_commands.py
+++ src/commands/profile_commands.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
-from models.user_profile import place_bet, enter_lottery, draw_lottery, get_leaderboard, display_leaderboard_embed, display_profile
+import random
+from models.user_profile import place_bet, get_leaderboard, display_leaderboard_embed, display_profile
 
 class ProfileCommands(commands.Cog):
     def __init__(self, bot):
@@ -17,24 +18,3 @@ class ProfileCommands(commands.Cog):
             await channel.send(embed=leaderboard_embed)
         else:
             await channel.send("No leaderboard data available.")
-
-    # Bet Command
-    @commands.command()
-    async def bet(self, ctx, amount: int):
-        response = await place_bet(ctx.author.id, amount)
-        await ctx.send(response)
-
-    # Lottery Command
-    @commands.command()
-    async def lottery(self, ctx, amount: int):
-        response = await enter_lottery(ctx.author.id, amount)
-        await ctx.send(response)
-
-    # Draw Lottery Command
-    @commands.command()
-    async def drawlottery(self, ctx):
-        if any(role.permissions.administrator for role in ctx.author.roles):
-            response = await draw_lottery()
-            await ctx.send(response)
-        else:
-            await ctx.send("You do not have permission to draw the lottery.")
